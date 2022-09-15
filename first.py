b7383a127088e4149ab0252d30898fd6ae34797c
Udaya Bhasker Mattupalli <mub.oct05@gmail.com>
2022-09-15 14:10:19 -0700

    Commit 1 Sep 15th 2022

--- first.py
+++ first.py
@@ -1,3 +1,5 @@
+from contextlib import nullcontext
+
 print("======Begin=====")
 a = 3
 b = "john"
@@ -45,8 +47,54 @@ def func():
 func()
 
 print(k)
+"""
 # j is local vairable here so we expect a NameError: name 'j' is not defined
 # if any exception  the flow stops and program exits.
-#print(j)
+print(j)
 # as k is global this is accessible
+"""
 print(k)
+
+a = """adfadsfafa
+adfadsfa
+adsfafda
+asdfaf
+adfaaf"""
+print("a==> ", a)
+
+X = "Hell0"
+print("1 Hello Type ==>", type(x))
+x = 20
+print("22 0 Type : ", type(x))
+x = 20.5
+print("3 20.5 Type : ", type(x))
+x = 1j
+print("ij Type : ", type(x))
+x = ["apple", "banana", "tomoto"]
+print("4 : Type : ", type(x))
+x = ("apple", "banana", "tomoto")
+print("5 : Type : ", type(x))
+x = range(6)
+print("6 : Type : ", type(x))
+x = {"name": "john", "age": 32}
+print("7 : Type : ", type(x))
+x = {"app", "app", "app"}
+print("8 : Type : ", type(x))
+x = frozenset(x)
+print("9 type : ", type(x))
+x = True
+print("10 type : ", type(x))
+x = b"Hellp"
+print("11 type : ", type(x))
+x = bytearray(10)
+print("12 type : ", type(x))
+x = None
+print("13 type : ", type(x))
+x = str("Hello")
+x = int(20)
+x = float(20.5)
+x = complex(1j)
+x = list(("apple", "banana"))
+x = tuple(("apple", "banana"))
+x = range(8)
+x = dict(name="jo", age=14)
